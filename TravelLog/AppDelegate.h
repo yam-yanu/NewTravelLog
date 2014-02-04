1887828741da6b7d8211aa14a42cf96c611fdb1c
yam-yanu <you1095@gmail.com>
2014-02-04 18:43:18 +0900

    Initial Commit

---
+++ TravelLog/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TravelLog
+//
+//  Created by ami on 2014/02/04.
+//  Copyright (c) 2014年 Yosuke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TravelLog/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TravelLog
+//
+//  Created by ami on 2014/02/04.
+//  Copyright (c) 2014年 Yosuke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
